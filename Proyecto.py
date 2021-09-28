325f4639d49b8650ebebab81829ed91330cba5bf
systemfucked <91567684+systemfucked@users.noreply.github.com>
2021-09-28 14:50:14 -0500

    Add files via upload

---
+++ Proyecto.py
@@ -0,0 +1,30 @@
+print("Tool Kali")
+print("created by: xnxx <3") 
+
+print("1 = lista de herramientas")
+print("2 = cerrar")
+
+Tools = int(input("Opcion: ") )
+if Tools % 2 == 1:
+    print("1.Th3Inspector")
+    print("2.DDos-Ripper")
+    print("3.fbi")
+    Tools_2 = int(input("Opcion: "))
+    if Tools_2 % 3 == 1:
+        print("¿Que es th3Inspector?")
+        print("es una herramienta de recopilación de información / information gathering de código abierto disponible en Github a través del cual puede encontrar fácilmente mucha información sobre un objetivo, como detalles del servidor, información de whois, IP de destino, número de teléfono, correo electrónico. etc")
+        print("1.instalar")
+        print("2.cerrar")
+        Tools_3 = int(input("Opcion: "))
+        if Tools_3 % 2 == 1:
+            print("https://github.com/Moham3dRiahi/Th3inspector.git").__init__
+
+    else:
+        print("¿Que es DDos-Ripper")
+        print("ES un servidor de ataque distribuible de denegación de servicio (DDOS) que corta los objetivos o la infraestructura circundante en una avalancha de tráfico de Internet Los ataques DDoS logran su eficacia utilizando múltiples sistemas informáticos comprometidos como fuente de tráfico de ataque. Los motores de búsqueda pueden incluir computadoras y otros recursos de red, como dispositivos de IoT. Desde un nivel superior, el ataque DDOS es como un atasco inesperado atascado en una carretera, que impide que el tráfico regular llegue a su destino.")
+        print("1.instalar")
+        print("2.cerrar")
+        Tools_4 = int(input("Opcion: "))
+        if Tools_4 % 2 == 1:
+            print("https://github.com/palahsu/DDoS-Ripper.git").__init__
+            
